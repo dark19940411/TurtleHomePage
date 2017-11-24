e2b2657f548bf52439fa84ba2b6a7c30bfe99e4d
dark19940411 <279714010@qq.com>
2017-11-24 12:04:39 +0800

    Add blogs list and article content template file to project

--- Turto/main.js
+++ Turto/main.js
@@ -7,6 +7,8 @@ var Generator = require('./Tools/generator');
     var argv = require("minimist")(process.argv.slice(2));
 
     if (argv._[0] == 'g' || argv._[0] == 'generate') {
+
+
         var generator = new Generator();
         generator.generate();
     }
--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,11 @@
  */
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-    // place code for your default task here
+gulp.task('Move-libraries', function () {
     gulp.src("Template/libraries")
         .pipe(gulp.dest("dist"));
+});
+
+gulp.task('Turto', function () {
+    require('Turto/main');
 });
