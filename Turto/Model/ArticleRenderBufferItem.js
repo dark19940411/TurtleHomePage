5fd9494cfb56a8203fb255ff2a1f3b2388dab23c
dark19940411 <279714010@qq.com>
2017-12-08 16:11:41 +0800

    Add ArticleRenderBufferItem for future usage.

---
+++ Turto/Model/ArticleRenderBufferItem.js
@@ -0,0 +1,9 @@
+function ArticleRenderBufferPoolItem(options) {
+    this.title = options.title;
+    this.date = option.date;
+    this.brev = options.brev;//摘要
+    this.formerItem = options.formerItem;
+    this.latterItem = options.latterItem;
+}
+
+module.exports = ArticleRenderBufferPoolItem;
