fb400875aa9ebac7eaaf437dcd92b465d17e203b
dark19940411 <279714010@qq.com>
2017-12-11 12:05:20 +0800

    Add formMainStructureRenderData method to BlogListPageViewModel

---
+++ Turto/ViewModel/BlogsListPageViewModel.js
@@ -0,0 +1,24 @@
+function BlogsListPageViewModel() {
+
+    // 生成渲染完整的博客列表页面所需要的数据
+    this.formMainStructureRenderData = function (renderBufItem, mainPanelContent, callback) {
+        var filerefpath = __buildingTemplateDir.stringByAppendingPathComponent('blogs_list_fileref');
+        fs.readFile(filerefpath, function (err, data) {
+            if (err) {
+                callBack(err);
+                return console.error(err);
+            }
+
+            var msdata = new MSData({
+                fileref: data.toString(),
+                title: renderBufItem.title,
+                mainPanel: mainPanelContent,
+                generatedContent: renderBufItem.content
+            });
+
+            callBack(null, msdata);
+        });
+    }
+}
+
+module.exports = BlogsListPageViewModel;
