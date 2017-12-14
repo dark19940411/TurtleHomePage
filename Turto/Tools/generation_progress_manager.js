ba91e8346ab1a119d6eb9cafa8949b8254a0f6be
dark19940411 <279714010@qq.com>
2017-12-14 11:33:16 +0800

    Finished GenerationProgressManager

--- Turto/Tools/generation_progress_manager.js
+++ Turto/Tools/generation_progress_manager.js
@@ -1,9 +1,39 @@
 function GenerationProgressManager() {
-    var blogsListPageCount = Math.ceil(articlesChain.length / __blogsPerPage);
+    var totalBlogsListPagesCount = Math.ceil(articlesChain.length / __blogsPerPage);
     var generatedBlogsListPagesCount = 0;
     var generatedBlogPostPagesCount = 0;
 
-    
+    this.signalOneBLPageCompleted = function () {
+        generatedBlogsListPagesCount++;
+    };
+
+    this.signalOneBPPageCompleted = function () {
+        generatedBlogPostPagesCount++;
+    };
+
+    this.blGenerationProgress = function () {
+        return generatedBlogsListPagesCount / totalBlogsListPagesCount;
+    };
+
+    this.bpGenerationProgress = function () {
+        return generatedBlogPostPagesCount / articlesChain.length;
+    };
+
+    this.totalProgress = function () {
+        return (generatedBlogPostPagesCount + generatedBlogsListPagesCount) / (totalBlogsListPagesCount + articlesChain.length);
+    };
+
+    this.isBlGenerationCompleted = function () {
+        return generatedBlogsListPagesCount === totalBlogsListPagesCount;
+    };
+
+    this.isBpGenerationCompleted = function () {
+        return generatedBlogPostPagesCount === articlesChain.length;
+    };
+
+    this.isGenerationCompleted = function () {
+        return this.isBlGenerationCompleted() && this.isBpGenerationCompleted();
+    };
 }
 
 module.exports = new GenerationProgressManager();
