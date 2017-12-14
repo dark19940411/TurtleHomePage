b0f8bdad8857d9cd0a4ff51ddde857cd6861a869
dark19940411 <279714010@qq.com>
2017-12-14 11:06:52 +0800

    Add properties to generation progress manager.

---
+++ Turto/Tools/generation_progress_manager.js
@@ -0,0 +1,9 @@
+function GenerationProgressManager() {
+    var blogsListPageCount = Math.ceil(articlesChain.length / __blogsPerPage);
+    var generatedBlogsListPagesCount = 0;
+    var generatedBlogPostPagesCount = 0;
+
+    
+}
+
+module.exports = new GenerationProgressManager();
