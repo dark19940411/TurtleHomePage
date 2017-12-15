85b8e00c9664eba5b419a21758bf159d91549485
dark19940411 <279714010@qq.com>
2017-12-15 16:41:16 +0800

    Add clear process to Turto

--- Turto/main.js
+++ Turto/main.js
@@ -4,11 +4,11 @@
 require('./Tools/utilities');
 
 var Generator = require('./Tools/Generator');
-var path = require('path');
 var fs = require('fs-extra');
 var task = require('./Tools/task');
 var ArticlesChainBuilder = require('./Tools/ArticlesChainBuilder');
 var distributor = require('./Tools/distributor');
+var path = require('path');
 
 var clearjobfinshed = false;
 var buildchainjobfinshed = false;
@@ -16,15 +16,9 @@ var buildchainjobfinshed = false;
 (function main() {
     var argv = require("minimist")(process.argv.slice(2));
 
-    if (argv._[0] === 'g' || argv._[0] === 'generate') {
+    if (argv._[0] === 'g' || argv._[0] === 'generate' || argv.generate) {   //生成网站流程
 
-        task.do('Clear last generated results', function (done) {
-            clearjob(function () {
-                clearjobfinshed = true;
-                done();
-                setupGenerationTask();
-            });
-        });
+        startClearProcess();
 
         task.do('build article chain', function (done) {
             var builder = new ArticlesChainBuilder();
@@ -36,7 +30,51 @@ var buildchainjobfinshed = false;
             });
         });
     }
+    else if (argv._[0] === 'c' || argv._[0] === 'clear' || argv.clear) {    //清理生成的内容流程
+        startClearProcess();
+    }
+    else if (argv._[0] === 'n' || argv._[0] === 'new' || argv.new) {    //新建文章流程
+        var title = null;
+        if (argv.new) {
+            title = argv.new;
+        }
+        else {
+            title = argv._[1];
+        }
+
+        var newarticlepath = __postdir.stringByAppendingPathComponent(title)
+            .stringByAppendingPathComponent(title + '.md');
+        if(fs.existsSync(newarticlepath)) {
+            return console.log('This article is already existed, please change its title...'.red);
+        }
+        else {
+            var ensureCount = 2;
+            var currentEnsureIndex = 0;
+            function createJobDoneCheck(err) {
+                if (err) {
+                    return console.error(err);
+                }
+                currentEnsureIndex++;
+                if (currentEnsureIndex === ensureCount) {
+                    console.log(String('Finished creating article "' + title + '" at ' + newarticlepath).green);
+                }
+            }
+
+            fs.ensureFile(newarticlepath, createJobDoneCheck);
+            fs.ensureDir(path.dirname(newarticlepath).stringByAppendingPathComponent('images'), createJobDoneCheck);
+        }
+    }
 }());
+
+function startClearProcess() {
+    task.do('Clear last generated results', function (done) {
+        clearjob(function () {
+            clearjobfinshed = true;
+            done();
+            setupGenerationTask();
+        });
+    });
+}
 
 function clearjob(callback) {
     var totalJobsCount = 4;
